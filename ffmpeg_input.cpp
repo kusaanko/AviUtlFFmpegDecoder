3eaf3c757e8cc015dcda1da39f2304857a9ad65a
kusaanko <39370373+kusaanko@users.noreply.github.com>
2020-12-26 01:25:48 +0900

    本体のバージョンを更新

--- ffmpeg_input.cpp
+++ ffmpeg_input.cpp
@@ -34,8 +34,8 @@ extern "C" {
 
 #define VIDEO_EXT "*.avi;*.mov;*.wmv;*.mp4;*.webm;*.mkv;*.flv;*.264;*.mpeg;*.ts;*.mts;*.m2ts;"
 #define AUDIO_EXT "*.mp3;*.ogg;*.wav;*.aac;*.wma;*.m4a;*.webm;*.opus;"
-#define VERSION "0.5"
-#define BUILD_NUM "26"
+#define VERSION "0.6"
+#define BUILD_NUM "51"
 
 
 //---------------------------------------------------------------------
